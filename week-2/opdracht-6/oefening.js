0fbb0bd7498cb4f5b7db7e4237446a0ae57956f0
jayden-rezigue <9029068@student.zadkine.nl>
2025-12-04 09:34:51 +0100

    opdracht_6

---
+++ week-2/opdracht-6/oefening.js
@@ -0,0 +1,8 @@
+function kleur_aanpassen(){
+document.getElementById("titel").style.color = "red";
+}
+function tekst_aanpassen(){
+document.getElementById("titel").innerHTML = "tekst is aangepast";
+}
+kleur_aanpassen();
+tekst_aanpassen();
