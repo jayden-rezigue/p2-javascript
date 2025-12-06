5ee39bc4473ad28b815311d65b50231d320b31f3
jayden-rezigue <9029068@student.zadkine.nl>
2025-12-06 22:29:36 +0100

    opdracht_10

---
+++ week-2/opdracht-10/script.js
@@ -0,0 +1,15 @@
+function groet_ochtend(){
+    document.getElementById("titel").innerHTML = "Goedemorgen!";
+    document.body.style.backgroundColor = "lightyellow";
+    document.body.style.color = "black";
+}
+function groet_middag(){
+    document.getElementById("titel").innerHTML = "Goedemiddag!";
+    document.body.style.backgroundColor = "lightblue";
+    document.body.style.color = "black";
+}
+function groet_avond(){
+    document.getElementById("titel").innerHTML = "Goedeavond!";
+    document.body.style.backgroundColor = "darkblue";
+    document.body.style.color = "white";
+}
