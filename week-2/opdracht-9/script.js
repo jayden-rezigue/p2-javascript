19e18ccdff88feffc4d02a5095d5a6531031acde
jayden-rezigue <9029068@student.zadkine.nl>
2025-12-06 19:30:21 +0100

    opdracht_9

---
+++ week-2/opdracht-9/script.js
@@ -0,0 +1,8 @@
+function verberg_tekst(){
+    document.getElementById("titel").style.display = "none";
+}
+function toon_tekst(){
+    document.getElementById("titel").style.display = "block";
+    document.getElementById("titel").style.backgroundColor = "lightyellow";
+}
+
