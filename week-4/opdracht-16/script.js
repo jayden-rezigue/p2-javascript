65ea6d9176d297d56e37cedf1c6c55bbf418cbab
jayden-rezigue <9029068@student.zadkine.nl>
2026-02-03 10:07:13 +0100

    opdracht_16

---
+++ week-4/opdracht-16/script.js
@@ -0,0 +1,13 @@
+let knop = document.getElementById("paragraaf");
+
+knop.addEventListener("click", maakRood);
+
+function maakRood() {
+  let p = document.getElementById("paragraaf");
+  p.style.color = "red";
+
+function veranderKleur(){
+document.style.backgroundColor = "lightblue";
+}
+
+}
